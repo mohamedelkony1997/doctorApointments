3913d751333197ff222891f2bb1e94557b6ff091
Mohamed Alqouny <mohamedelkony1997>
2025-04-28 10:58:52 +0400

    signup api -models -cubit screen

--- lib/core/networking/api_services.dart
+++ lib/core/networking/api_services.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:doc/Features/login/data/models/login_request_body.dart';
 import 'package:doc/Features/login/data/models/login_response.dart';
+import 'package:doc/Features/signUp/data/models/sign_up_request_body.dart';
+import 'package:doc/Features/signUp/data/models/sign_up_response.dart';
 import 'package:doc/core/networking/api_constants.dart';
 import 'package:retrofit/retrofit.dart';
 part 'api_services.g.dart';
@@ -12,5 +14,9 @@ abstract class ApiService {
   @POST(ApiConstants.login)
   Future<LoginResponse> login(
     @Body() LoginRequestBody loginRequestBody,
+  );
+  @POST(ApiConstants.signup)
+  Future<SignupResponse> signup(
+    @Body() SignupRequestBody signupRequestBody,
   );
 }
